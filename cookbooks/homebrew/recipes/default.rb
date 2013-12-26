2e56ad53c40d7da5e776ce668f9b8712af972c56
Jun Yokoyama <jun@larus.org>
2013-12-27 04:23:34 +0900

    fixed wrong url

--- cookbooks/homebrew/recipes/default.rb
+++ cookbooks/homebrew/recipes/default.rb
@@ -1,7 +1,7 @@
 homebrew_go = "#{Chef::Config[:file_cache_path]}/homebrew_go"
 
 remote_file homebrew_go do
-  source "https://raw.github.com/mxcl/homebrew/go"
+  source "https://raw.github.com/mxcl/homebrew/go/install"
   mode 00755
 end
 
